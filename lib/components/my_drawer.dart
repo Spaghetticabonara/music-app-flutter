d7b956ff14e7eee5183baded99574e6e6f50224c
chanita <chanita.tdev@gmail.com>
2024-05-22 22:40:46 +0700

    get file

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:music_player_app/pages/setting_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          // logo
+          DrawerHeader(
+            child: Center(
+              child: Icon(
+                Icons.music_note,
+                size: 40,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+
+          // home tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: const Text("HOME"),
+              leading: const Icon(Icons.home),
+              textColor: Theme.of(context).colorScheme.inversePrimary,
+              iconColor: Theme.of(context).colorScheme.inversePrimary,
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+
+          // settings tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: const Text("SETTINGS"),
+              leading: const Icon(Icons.settings),
+              textColor: Theme.of(context).colorScheme.inversePrimary,
+              iconColor: Theme.of(context).colorScheme.inversePrimary,
+              onTap: () {
+                Navigator.pop(context);
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SettingsPage(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/models/playlist_provider.dart
@@ -0,0 +1,155 @@
+import 'dart:developer';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:music_player_app/models/song.dart';
+
+class PlaylistProvider extends ChangeNotifier {
+  // playlist of songs
+  final List<Song> _playlist = [
+    // song 1
+    Song(
+      songName: "Watch The World Burn",
+      artistName: 'Falling In Reverse',
+      albumArtImagePath: 'assets/images/album_artwork_1.png',
+      audioPath: 'audio/watch_the_world_burn.mp3',
+    ),
+
+    // song 2
+    Song(
+      songName: "Neon Grave",
+      artistName: 'Dayseeker',
+      albumArtImagePath: 'assets/images/album_artwork_2.png',
+      audioPath: 'audio/neon_grave.mp3',
+    ),
+  ];
+  // current song playing index
+  int? _currentSongIndex;
+
+  /* AUDIO PLAYER */
+
+  // audio player
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  // duration
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+
+  // constructor
+  PlaylistProvider() {
+    listenToDuration();
+  }
+
+  // initially not playing
+  bool _isPlaying = false;
+
+  // play the song
+  void play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
+    await _audioPlayer.stop();
+    await _audioPlayer.play(AssetSource(path));
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  // pause current song
+  void pause() async {
+    await _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  // resume playing
+  void resume() async {
+    await _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  // pause or resume
+  void pauseOrResume() async {
+    if (_isPlaying) {
+      pause();
+    } else {
+      resume();
+    }
+    notifyListeners();
+  }
+
+  // seek to a specific position in the current song
+  void seek(Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  // play next song
+  void playNextSong() {
+    if (_currentSongIndex != null) {
+      if (_currentSongIndex! < _playlist.length - 1) {
+        // go to the next song if it's not the last song
+        currentSongIndex = _currentSongIndex! + 1;
+      } else {
+        // if it's the last song, loop back to the first song
+        currentSongIndex = 0;
+      }
+    }
+  }
+
+  // play previous song
+  void playPreviousSong() async {
+    // if more than 2 secs have passed, restart the current song
+    if (_currentDuration.inSeconds > 2) {
+      _currentDuration = Duration.zero;
+    }
+    // if it's within first 2 seconds of the song, go to previous song
+    else {
+      if (_currentSongIndex! > 0) {
+        currentSongIndex = _currentSongIndex! - 1;
+      } else {
+        // if it's the first song, loop back to last song
+        currentSongIndex = _playlist.length - 1;
+      }
+    }
+  }
+
+  // list to duration
+  void listenToDuration() {
+    // listen for total duration
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    // listen for current duration
+    _audioPlayer.onDurationChanged.listen((newPosition) {
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+
+    // listen for song completion
+    _audioPlayer.onDurationChanged.listen((newPosition) {
+      playNextSong();
+    });
+  }
+
+  // dispose audio player
+
+  // getters
+  List<Song> get playlist => _playlist;
+  int? get currentSongIndex => _currentSongIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
+
+  // setters
+  set currentSongIndex(int? newIndex) {
+    // update current song index
+    _currentSongIndex = newIndex;
+
+    if (newIndex != null) {
+      play(); // play the song at the new index
+    }
+
+    // update UI
+    notifyListeners();
+  }
+}
